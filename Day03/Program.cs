da49f85e7d4f72dc9a95e6bcc722b3455329fe7c
Daniel <d.o.rasmusson@gmail.com>
2023-12-05 14:12:04 +0100

    Started on day 3 part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -3,6 +3,10 @@
 var input = File.ReadAllLines("Input.txt");
 
 var one = PartOne(input);
+Console.WriteLine(one);
+
+var two = PartTwo(input);
+Console.WriteLine(two);
 
 int PartOne(string[] input)
 {
@@ -12,21 +16,86 @@ int PartOne(string[] input)
     {
         for (int x = 0; x < input[y].Length; x++)
         {
-            if (IsCharPart(input[y][x]))
+            if (IsPartSymbol(input[y][x]))
                 parts.Add(new Coord(x, y));
 
             if (int.TryParse(input[y][x].ToString(), out _))
             {
-                var value = int.Parse(input[y].Substring(x, 3));
+                var endIndex = x;
+                while(endIndex < input[y].Length && char.IsDigit(input[y][endIndex])) endIndex++;
+
+                var numberLength = endIndex - x;
+                var value = int.Parse(input[y].Substring(x, numberLength));
                 numbers.Add(new Number(x, y, value));
-                x += 2;
+                x += numberLength - 1;
             }
         }
     }
-    return 0;
+
+    foreach (var part in parts)
+    {
+        foreach (var coord in part.GetNeighbours())
+        {
+            foreach (var number in numbers)
+            {
+                if (number.Coords.Any(x => x == coord)) number.Touched = true;
+            }
+        }
+    }
+
+    var result = numbers.Where(x => x.Touched).Sum(y => y.Value);
+
+    return result;
 }
 
-bool IsCharPart(char v) => !int.TryParse(v.ToString(), out _) && v != '.';
+int PartTwo(string[] input)
+{
+    var gearCandidates = new List<Coord>();
+    var numbers = new List<Number>();
+    for (int y = 0; y < input.Length; y++)
+    {
+        for (int x = 0; x < input[y].Length; x++)
+        {
+            if (IsGearSymbol(input[y][x]))
+                gearCandidates.Add(new Coord(x, y));
+
+            if (int.TryParse(input[y][x].ToString(), out _))
+            {
+                var endIndex = x;
+                while(endIndex < input[y].Length && char.IsDigit(input[y][endIndex])) endIndex++;
+
+                var numberLength = endIndex - x;
+                var value = int.Parse(input[y].Substring(x, numberLength));
+                numbers.Add(new Number(x, y, value));
+                x += numberLength - 1;
+            }
+        }
+    }
+
+    var gearRatio = 0;
+
+    foreach (var gearCandidate in gearCandidates)
+    {
+        foreach (var coord in gearCandidate.GetNeighbours())
+        {
+            foreach (var number in numbers)
+            {
+                if (number.Coords.Any(x => x == coord)) number.Touched = true;
+            }
+        }
+        if (numbers.Count(x => x.Touched) == 2)
+        {
+            var ratio = numbers.First(x => x.Touched).Value * numbers.Last(x => x.Touched).Value;
+            gearRatio += ratio;
+        }
+        foreach (var number in numbers) number.Touched = false;
+    }
+
+    return gearRatio;
+}
+
+bool IsPartSymbol(char v) => !int.TryParse(v.ToString(), out _) && v != '.';
+bool IsGearSymbol(char v) => v == '*';
 
 record Number
 {
@@ -39,4 +108,17 @@ record Number
     public int Value { get; set; }
     public bool Touched { get; set; }
 }
-struct Coord(int X, int Y);
+record Coord(int X, int Y)
+{
+    internal IEnumerable<Coord> GetNeighbours()
+    {
+        yield return this with { X = X - 1, Y = Y - 1 };
+        yield return this with { Y = Y - 1 };
+        yield return this with { X = X + 1, Y = Y - 1 };
+        yield return this with { X = X - 1 };
+        yield return this with { X = X + 1 };
+        yield return this with { X = X - 1, Y = Y + 1 };
+        yield return this with { Y = Y + 1 };
+        yield return this with { X = X + 1, Y = Y + 1 };
+    }
+}
